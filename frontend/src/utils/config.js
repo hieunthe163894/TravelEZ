4f1e4cf94610560b9acc8b61d16c43a7bf335508
Nguyễn Minh Quang <quangnguyen3922@gmail.com>
2024-06-18 21:44:10 +0700

    add and comment confix

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,2 +1,3 @@
 export const BASE_URL = 'http://localhost:8000/api/v1'
 // export const BASE_URL = 'http://mern-travel-booking-api.vercel.app/'
+// export const BASE_URL = 'https://booking-restaurant-zo7p.onrender.com/api/v1'
