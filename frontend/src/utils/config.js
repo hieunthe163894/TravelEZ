e665f9343e4ba7747ef2006dda7ad73ece1a545a
hieunthe163894 <hieunthe163894@fpt.edu.vn>
2024-06-23 22:21:06 +0700

    Hieu

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'http://localhost:8000/api/v1'
+// export const BASE_URL = 'http://localhost:8000/api/v1'
 // export const BASE_URL = 'http://mern-travel-booking-api.vercel.app/'
-// export const BASE_URL = 'https://travelez-4ro8.onrender.com/api/v1'
+export const BASE_URL = 'https://travelez-4ro8.onrender.com/api/v1'
