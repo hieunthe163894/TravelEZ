76bfd39496da731f51c9cb2fd5eac69712d9aaf3
hieunthe163894 <hieunthe163894@fpt.edu.vn>
2024-06-24 14:52:19 +0700

    fixBooking

--- backend/Controllers/bookingController.js
+++ backend/Controllers/bookingController.js
@@ -7,7 +7,7 @@ export const createBooking = async (req, res) => {
    
    try {
       const savedBooking = await newBooking.save()
-
+      
       res.status(200).json({ success: true, message: "Your tour is booked!", data: savedBooking })
    } catch (error) {
       res.status(500).json({ success: true, message: "Internal server error!" })
