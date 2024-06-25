b195e60de88ffbab645412e27ccc0ea5c26b90b6
hieunthe163894 <hieunthe163894@fpt.edu.vn>
2024-06-25 15:46:15 +0700

    Mịa

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,7 +6,6 @@ import heroImg02 from "../assets/images/hero-img02.jpg";
 import heroVideo from "../assets/images/hero-video.mp4";
 import worldImg from "../assets/images/world.png";
 import experienceImg from "../assets/images/experience.png";
-import ImageQC from "../assets/images/quangcao.jpg";
 
 import Subtitle from "./../shared/subtitle";
 import SearchBar from "./../shared/SearchBar";
@@ -25,7 +24,7 @@ const Home = () => {
         target="_blank"
       >
         <img
-          src={ImageQC}
+          src={"../assets/images/quangcao.jpg"}
           alt="Ad Left"
           className="ad-img position-fixed top-50 start-0 translate-middle-y"
           style={{
@@ -42,7 +41,7 @@ const Home = () => {
         target="_blank"
       >
         <img
-          src={ImageQC}
+          src={"../assets/images/quangcao.jpg"}
           alt="Ad Right"
           className="ad-img position-fixed top-50 end-0 translate-middle-y"
           style={{
@@ -67,156 +66,158 @@ const Home = () => {
         />
       </a> */}
       {/* ========== HERO SECTION ========== */}
-      <div style={{marginLeft :"75px", marginRight:"75px"}}>
-      <section>
-        <Container>
-          <Row>
-            <Col lg="6">
-              <div className="hero__content">
-                <div className="hero__subtitle d-flex align-items-center">
-                  <Subtitle subtitle={"Biết trước khi bạn đặt bàn"} />
-                  <img src={worldImg} alt="" />
+      <div style={{ marginLeft: "75px", marginRight: "75px" }}>
+        <section>
+          <Container>
+            <Row>
+              <Col lg="6">
+                <div className="hero__content">
+                  <div className="hero__subtitle d-flex align-items-center">
+                    <Subtitle subtitle={"Biết trước khi bạn đặt bàn"} />
+                    <img src={worldImg} alt="" />
+                  </div>
+                  <h2>
+                    Thưởng thức hương vị tinh tế và không gian sang trọng tại
+                    các nhà hàng hàng đầu Nhà hàng hàng đầu
+                  </h2>
+                  <p>
+                    Bắt tay vào hành trình ẩm thực của bạn với chúng tôi! Tại
+                    đây, bạn sẽ khám phá những hương vị mới và độc đáo, cùng với
+                    bầu không khí thoải mái và dịch vụ chuyên nghiệp. Hãy đặt
+                    bàn ngay hôm nay và chuẩn bị cho trải nghiệm ẩm thực đặc
+                    biệt cùng bạn bè và gia đình.
+                  </p>
                 </div>
-                <h2>
-                  Thưởng thức hương vị tinh tế và không gian sang trọng tại các
-                  nhà hàng hàng đầu Nhà hàng hàng đầu
+              </Col>
+
+              <Col lg="2">
+                <div className="hero__img-box">
+                  <img src={heroImg} alt="" />
+                </div>
+              </Col>
+              <Col lg="2">
+                <div className="hero__img-box hero__video-box mt-4">
+                  <video src={heroVideo} alt="" controls />
+                </div>
+              </Col>
+              <Col lg="2">
+                <div className="hero__img-box mt-5">
+                  <img src={heroImg02} alt="" />
+                </div>
+              </Col>
+
+              <SearchBar />
+            </Row>
+          </Container>
+        </section>
+        {/* ============================================================== */}
+
+        {/* ==================== HERO SECTION START ====================== */}
+        <section>
+          <Container>
+            <Row>
+              <Col lg="3">
+                <h5 className="services__subtitle">
+                  Những gì chúng tôi phục vụ
+                </h5>
+                <h2 className="services__title">
+                  Chúng tôi cung cấp dịch vụ tốt nhất của chúng tôi
                 </h2>
-                <p>
-                  Bắt tay vào hành trình ẩm thực của bạn với chúng tôi! Tại đây,
-                  bạn sẽ khám phá những hương vị mới và độc đáo, cùng với bầu
-                  không khí thoải mái và dịch vụ chuyên nghiệp. Hãy đặt bàn ngay
-                  hôm nay và chuẩn bị cho trải nghiệm ẩm thực đặc biệt cùng bạn
-                  bè và gia đình.
-                </p>
-              </div>
-            </Col>
+              </Col>
+              <ServiceList />
+            </Row>
+          </Container>
+        </section>
 
-            <Col lg="2">
-              <div className="hero__img-box">
-                <img src={heroImg} alt="" />
-              </div>
-            </Col>
-            <Col lg="2">
-              <div className="hero__img-box hero__video-box mt-4">
-                <video src={heroVideo} alt="" controls />
-              </div>
-            </Col>
-            <Col lg="2">
-              <div className="hero__img-box mt-5">
-                <img src={heroImg02} alt="" />
-              </div>
-            </Col>
+        {/* ========== FEATURED TOUR SECTION START ========== */}
+        <section>
+          <Container>
+            <Row>
+              <Col lg="12" className="mb-5">
+                <Subtitle subtitle={"Explore"} />
+                <h2 className="featured__tour-title">Nhà hàng nổi bật</h2>
+              </Col>
+              <FeaturedTourList />
+            </Row>
+          </Container>
+        </section>
+        {/* ========== FEATURED TOUR SECTION END =========== */}
 
-            <SearchBar />
-          </Row>
-        </Container>
-      </section>
-      {/* ============================================================== */}
+        {/* ========== EXPERIENCE SECTION START ============ */}
+        <section>
+          <Container>
+            <Row>
+              <Col lg="6">
+                <div className="experience__content">
+                  <Subtitle subtitle={"Kinh nghiệm"} />
+                  <h2>
+                    Với tất cả kinh nghiệm của chúng tôi <br /> Chúng tôi sẽ
+                    phục vụ bạn
+                  </h2>
+                  <p>
+                    Trang web đặt bàn của chúng tôi là địa chỉ tin cậy để bạn
+                    đặt chỗ tại nhà hàng một cách thuận tiện và nhanh chóng, đảm
+                    bảo dịch vụ chất lượng.
+                  </p>
+                </div>
 
-      {/* ==================== HERO SECTION START ====================== */}
-      <section>
-        <Container>
-          <Row>
-            <Col lg="3">
-              <h5 className="services__subtitle">Những gì chúng tôi phục vụ</h5>
-              <h2 className="services__title">
-                Chúng tôi cung cấp dịch vụ tốt nhất của chúng tôi
-              </h2>
-            </Col>
-            <ServiceList />
-          </Row>
-        </Container>
-      </section>
+                <div className="counter__wrapper d-flex align-items-center gap-5">
+                  <div className="counter__box">
+                    <span>12k+</span>
+                    <h6>Đặt bàn thành công</h6>
+                  </div>
+                  <div className="counter__box">
+                    <span>2k+</span>
+                    <h6>Khách hàng thường xuyên</h6>
+                  </div>
+                </div>
+              </Col>
+              <Col lg="6">
+                <div className="experience__img">
+                  <img src={experienceImg} alt="" />
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </section>
+        {/* ========== EXPERIENCE SECTION END ============== */}
 
-      {/* ========== FEATURED TOUR SECTION START ========== */}
-      <section>
-        <Container>
-          <Row>
-            <Col lg="12" className="mb-5">
-              <Subtitle subtitle={"Explore"} />
-              <h2 className="featured__tour-title">Nhà hàng nổi bật</h2>
-            </Col>
-            <FeaturedTourList />
-          </Row>
-        </Container>
-      </section>
-      {/* ========== FEATURED TOUR SECTION END =========== */}
-
-      {/* ========== EXPERIENCE SECTION START ============ */}
-      <section>
-        <Container>
-          <Row>
-            <Col lg="6">
-              <div className="experience__content">
-                <Subtitle subtitle={"Kinh nghiệm"} />
-                <h2>
-                  Với tất cả kinh nghiệm của chúng tôi <br /> Chúng tôi sẽ phục
-                  vụ bạn
+        {/* ========== GALLERY SECTION START ============== */}
+        <section>
+          <Container>
+            <Row>
+              <Col lg="12">
+                <Subtitle subtitle={"Phòng trưng bày"} />
+                <h2 className="gallery__title">
+                  Ghé thăm đánh giá của khách hàng của chúng tôi
                 </h2>
-                <p>
-                  Trang web đặt bàn của chúng tôi là địa chỉ tin cậy để bạn đặt
-                  chỗ tại nhà hàng một cách thuận tiện và nhanh chóng, đảm bảo
-                  dịch vụ chất lượng.
-                </p>
-              </div>
+              </Col>
+              <Col lg="12">
+                <MasonryImagesGallery />
+              </Col>
+            </Row>
+          </Container>
+        </section>
+        {/* ========== GALLERY SECTION END ================ */}
 
-              <div className="counter__wrapper d-flex align-items-center gap-5">
-                <div className="counter__box">
-                  <span>12k+</span>
-                  <h6>Đặt bàn thành công</h6>
-                </div>
-                <div className="counter__box">
-                  <span>2k+</span>
-                  <h6>Khách hàng thường xuyên</h6>
-                </div>
-              </div>
-            </Col>
-            <Col lg="6">
-              <div className="experience__img">
-                <img src={experienceImg} alt="" />
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </section>
-      {/* ========== EXPERIENCE SECTION END ============== */}
-
-      {/* ========== GALLERY SECTION START ============== */}
-      <section>
-        <Container>
-          <Row>
-            <Col lg="12">
-              <Subtitle subtitle={"Phòng trưng bày"} />
-              <h2 className="gallery__title">
-                Ghé thăm đánh giá của khách hàng của chúng tôi
-              </h2>
-            </Col>
-            <Col lg="12">
-              <MasonryImagesGallery />
-            </Col>
-          </Row>
-        </Container>
-      </section>
-      {/* ========== GALLERY SECTION END ================ */}
-
-      {/* ========== TESTIMONIAL SECTION START ================ */}
-      <section>
-        <Container>
-          <Row>
-            <Col lg="12">
-              <Subtitle subtitle={"Người hâm mộ yêu thích"} />
-              <h2 className="testimonial__title">
-                Người hâm mộ nói gì về chúng tôi
-              </h2>
-            </Col>
-            <Col lg="12">
-              <Testimonials />
-            </Col>
-          </Row>
-        </Container>
-      </section>
-      {/* ========== TESTIMONIAL SECTION END ================== */}
-      <NewsLetter />
+        {/* ========== TESTIMONIAL SECTION START ================ */}
+        <section>
+          <Container>
+            <Row>
+              <Col lg="12">
+                <Subtitle subtitle={"Người hâm mộ yêu thích"} />
+                <h2 className="testimonial__title">
+                  Người hâm mộ nói gì về chúng tôi
+                </h2>
+              </Col>
+              <Col lg="12">
+                <Testimonials />
+              </Col>
+            </Row>
+          </Container>
+        </section>
+        {/* ========== TESTIMONIAL SECTION END ================== */}
+        <NewsLetter />
       </div>
     </>
   );
